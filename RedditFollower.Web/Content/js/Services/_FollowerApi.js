b7fde19dd622aace74e62f0c9314f3709ad7448b
MattSegal <mattdsegal@gmail.com>
2016-09-26 21:00:47 +1000

    fix relative path bug in AJAX request

--- RedditFollower.Web/Content/js/Services/_FollowerApi.js
+++ RedditFollower.Web/Content/js/Services/_FollowerApi.js
@@ -4,7 +4,7 @@
             successCallback({ users: [], threads: [] })
             return
         }
-        uri = "http://localhost/api/reddit/threads"
+        uri = "/api/reddit/threads"
         method = 'POST'
         console.log("Querying follower api...")
         var httpRequest = new XMLHttpRequest()
